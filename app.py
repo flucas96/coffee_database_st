b430bcf27c3ac5911d1a1a8eca2e2fcbed0f8580
Fabian Lucas <fl@msr.intern@msranalyse.msr.de>
2024-02-13 10:21:56 +0100

    push

--- app.py
+++ app.py
@@ -2,6 +2,27 @@ import streamlit as st
 import pandas as pd
 import connectors
 import streamlit_antd_components as sac
+from st_mui_table import st_mui_table
+import base64
+from PIL import Image
+import io
+
+def convert_image_to_base64(image):
+    """
+    Converts a PIL Image to a base64 encoded string, including the necessary HTML format prefix.
+
+    Parameters:
+    - image: PIL.Image - The image to be converted.
+
+    Returns:
+    - str: A complete HTML string that displays the base64 encoded image.
+    """
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")  # You can change format to JPEG or other types if needed
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    # Create the HTML string with the base64 encoded image
+    html_img_str = f'<img src="data:image/png;base64,{img_str}" style="max-height: 200px;" />'
+    return html_img_str
 
 st.set_page_config(page_title="Kaffee Datenbank", page_icon="☕", layout="wide")
 
@@ -10,7 +31,6 @@ st.set_page_config(page_title="Kaffee Datenbank", page_icon="☕", layout="wide"
 st.header("Unsere Kaffee Datenbank!")
 df = connectors.download_data("test_db_fl")
 
-st.write(df)
 with st.expander("**Neuen Kaffee hinzufügen**"):
     with st.form(key="new_coffee"):
 
@@ -36,23 +56,27 @@ with st.expander("**Neuen Kaffee hinzufügen**"):
 
         if new_coffee_submit:
             #check if all fields are filled
-            if name and haendler and preis and mahlgrad and bewertung and kommentar:
+            if name and haendler and preis and mahlgrad and bewertung and kommentar and image:
                 #add new coffee to database
+                image = Image.open(image)
+                image = convert_image_to_base64(image)
+
                 new_coffee = pd.DataFrame({
                     "Name": [name],
                     "Händler": [haendler],
                     "Preis": [preis],
                     "Mahlgrad": [mahlgrad],
                     "Bewertung": [bewertung],
-                    "Kommentar": [kommentar]
+                    "Kommentar": [kommentar],
+                    "Bild": [image]
                 })
                 connectors.add_coffee(new_coffee)
+                st.rerun()
             else:
                 st.toast("Bitte falle Felder ausfüllen :)")
                 st.error("Bitte falle Felder ausfüllen :)")
       
       
-
 
 st.header("Alle Einträge:")
 if len(df) == 0:
@@ -60,5 +84,63 @@ if len(df) == 0:
     sac.alert(label='<b>Noch keine Einträge :)<b>', size='lg', banner=True, icon=True, closable=True)
 
 else:
-    pass
+    df["Hinzugefügt"] = df["Hinzugefügt"].dt.strftime("%d.%m.%Y %H:%M:%S")
+    for col in df.columns:
+        df.rename(columns={col: f"<b>{col}<b>"}, inplace=True)
+
+    clicked_coffee = st_mui_table(
+    df,  return_clicked_cell=True
+)
+    st.write(clicked_coffee)
+
+    if clicked_coffee:
+        #clicked row
+        row = clicked_coffee["row"]
+        clicked_coffee = df.iloc[[row]]
+        clicked_coffee.columns = [col.replace("<b>", "").replace("</b>", "") for col in clicked_coffee.columns]
     #Daten anzeigen
+        with st.form(key="show_coffee"):
+            col_left, mid, col_right = st.columns([1,1,1])
+            with col_left:
+                name = st.text_input("Name", value=clicked_coffee["Name"].values[0])
+                st.markdown("<br>", unsafe_allow_html=True)
+                mahlgrad = st.text_input("Mahlgrad", value=clicked_coffee["Mahlgrad"].values[0])
+            with col_right:
+                haendler = st.text_input("Händler", value=clicked_coffee["Händler"].values[0])
+                preis = st.text_input("Preis", value=clicked_coffee["Preis"].values[0])
+
+            bewertung = st.slider("Bewertung", 0, 10, int(clicked_coffee["Bewertung"].values[0]))
+            kommentar = st.text_area("Kommentar", value=clicked_coffee["Kommentar"].values[0])
+            st.markdown("<br>", unsafe_allow_html=True)
+            _,middle,_ = st.columns([1,5,1])
+            middle.markdown("<center>" + clicked_coffee["Bild"].values[0] + "</center>", unsafe_allow_html=True)
+            col1,col2 = st.columns([1,1])
+            with col1:
+                save_btn = st.form_submit_button("Speichern", use_container_width=True, type="primary")
+            with col2:
+                del_btn = st.form_submit_button("Löschen", use_container_width=True)
+
+            if save_btn:
+                #save changes
+                #Make da dictionary of all changes
+                changes = {
+                }
+                if name != clicked_coffee["Name"].values[0]:
+                    changes["Name"] = name
+                if haendler != clicked_coffee["Händler"].values[0]:
+                    changes["Händler"] = haendler
+                if preis != clicked_coffee["Preis"].values[0]:
+                    changes["Preis"] = preis
+                if mahlgrad != clicked_coffee["Mahlgrad"].values[0]:
+                    changes["Mahlgrad"] = mahlgrad
+                if bewertung != clicked_coffee["Bewertung"].values[0]:
+                    changes["Bewertung"] = bewertung
+                if kommentar != clicked_coffee["Kommentar"].values[0]:
+                    changes["Kommentar"] = kommentar
+                if changes:
+                    connectors.update_coffee(clicked_coffee["id"].values[0], changes)
+                    st.rerun()
+
+            if del_btn:
+                connectors.delete_coffee(clicked_coffee["id"].values[0])
+                st.rerun()
--- connectors.py
+++ connectors.py
@@ -15,6 +15,17 @@ def add_coffee(df):
     coffee_table = "test_db_fl"
     upload_data(df,coffee_table)
 
+def delete_coffee(id):
+    coffee_table = "test_db_fl"
+    sql_code = f"DELETE FROM {coffee_table} WHERE id = {id}"
+    execute_sql_code(sql_code)
+
+def update_coffee(id,changes:dict):
+    cofee_table = "test_db_fl"
+    for key in changes.keys():
+        sql_code = f"UPDATE {cofee_table} SET `{key}` = '{changes[key]}' WHERE `id` = {id}"
+        execute_sql_code(sql_code)
+
 def upload_data(df,table,if_exists='append',index=False):
     """
     df: hochzuladener df
@@ -70,6 +81,74 @@ def upload_data(df,table,if_exists='append',index=False):
     server.stop()
 
 
+def execute_sql_code(sql_code,max_tries = 5):
+    """
+    Execute SQL Code
+    user = Username SQL Log in
+    paswword = Password SQL Log in
+    db = database name (vgh13,DLE23..)
+    sql_code = SQL Code
+    """
+    counter = 0
+    while True:
+        
+        try:
+        # Zugangsdaten
+            user = st.secrets["db_user"]
+            password = st.secrets["db_pass"]
+            ip = st.secrets["db_ip"]
+            port = 3306
+            db = st.secrets["db_name"]
+            ssh_user = st.secrets["ssh_user"]
+            ssh_password= st.secrets["ssh_pass"]
+
+            key = io.StringIO(st.secrets["ssh_key"])
+            key = paramiko.RSAKey.from_private_key(key, password=ssh_password)
+
+            # DF erstellen
+
+            # SSH Tunnel aufbauen
+            server = SSHTunnelForwarder(
+                ip,
+                ssh_username=ssh_user,
+                ssh_password=ssh_password,
+                remote_bind_address=('127.0.0.1', port),
+                ssh_pkey=key,
+            )
+
+            server.start()
+
+            #print(server.local_bind_port)  # show assigned local port
+            # Ab hier besteht die Verbindung und wir können auf die Datenbank zugreifen
+
+            engine = create_engine('mysql+pymysql://'+user+':'+password+'@localhost:'+str(server.local_bind_port)+'/'+db+'?charset=UTF8MB4')
+            #df.to_sql(con=engine,name='test',if_exists='append')
+            #engine.dispose() #Notwendig, sonst kann der Tunnel nicht geschlossen werden
+            #server.stop() # Schließen des Tunnels. Kann bei Bedarf hier passieren oder zu einem späteren Zeitpunkt
+            print("Successful")
+            break
+        except Exception as e: 
+            counter +=1
+            if counter >= max_tries:
+                return "Connection Failed"
+
+            print(f"{counter} - Try again - {e}")
+            sleep(5)
+            pass
+    
+    connection = engine.raw_connection()
+    with connection.cursor() as cursor:
+
+        cursor.execute(sql_code)
+        c = cursor
+
+    connection.commit()
+
+    # connection.close()
+    # engine.dispose()
+    connection.close()
+
+    return c
 
 def download_data(table,max_tries = 5):
     """
